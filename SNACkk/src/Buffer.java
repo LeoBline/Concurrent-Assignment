084f5fb8bf2bf69ac45463ee0fdbf7bf6e58e64d
Yanxi Ke <2581570217@qq.com>
2020-04-14 12:12:09 +0800

    Buffer update

--- SNACkk/src/Buffer.java
+++ SNACkk/src/Buffer.java
@@ -1,4 +1,25 @@
+package SNACkk.src;
+
+import com.sun.source.util.TaskEvent;
 
 public class Buffer {
+
+private char[] MovementList;
+private int Inptr;
+public Buffer() {
+		super();
+		MovementList = new char[40];
+		Inptr = 0;
+	}
+public char Take() {
+	char a =  MovementList[Inptr];
+	MovementList[Inptr] = ' ';
+	Inptr = Inptr - 1;
+	return a;
+}
+public void append(char a) {
+	Inptr = Inptr + 1;
+	MovementList[Inptr] = a;
+}
 
 }
