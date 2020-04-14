8472c5f05515c3320d832afdb9e781f5cb4eb95e
Yanxi Ke <2581570217@qq.com>
2020-04-14 12:16:12 +0800

    Buffer update 2

--- SNACkk/src/Buffer.java
+++ SNACkk/src/Buffer.java
@@ -1,25 +1,32 @@
 package SNACkk.src;
-
-import com.sun.source.util.TaskEvent;
-
 public class Buffer {
-
+//MovementList used to stord the palyer's move.Thread will take it and update the snake.
 private char[] MovementList;
+//Inptre used to record the append and take position.
 private int Inptr;
 public Buffer() {
 		super();
+		//it can store 40 chars.
 		MovementList = new char[40];
 		Inptr = 0;
 	}
+//we use ' ' present null.
 public char Take() {
+	if(Inptr != 0) {
 	char a =  MovementList[Inptr];
 	MovementList[Inptr] = ' ';
 	Inptr = Inptr - 1;
 	return a;
+	}
+	else {
+		return ' ';
+	}
 }
 public void append(char a) {
+	if(Inptr < 40) {
 	Inptr = Inptr + 1;
 	MovementList[Inptr] = a;
+	}
 }
 
 }
