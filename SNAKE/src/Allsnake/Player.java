e8bb43be7d9650d64c379576bbb7f1cff65d099b
WangJiaCheng1999 <yojc1999@gmail.com>
2020-04-23 11:27:18 +0800

    Basic information for Player.java (Adding Id and Score to Player.java)

---
+++ SNAKE/src/Allsnake/Player.java
@@ -0,0 +1,40 @@
+package Allsnake;
+
+public class Player implements Runnable{
+    private String ID;
+    private int Score;
+
+    public void run() {
+
+    }
+
+    public void InputMove(char move){
+
+    }
+
+    public void InitSnake(){
+
+    }
+
+    //Getter and setter
+    public int getScore() {
+        return Score;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+
+
+
+
+}
--- SNAKE/src/Allsnake/Server.java
+++ SNAKE/src/Allsnake/Server.java
@@ -1,24 +1,5 @@
 package Allsnake;
 
-// Simple snake code from https://code.google.com/p/java-snake/source/browse/trunk/javasnake/src/snake/Main.java
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.awt.image.BufferStrategy;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- * @author Peuch
- */
 public class Server {
-//
+
 }
