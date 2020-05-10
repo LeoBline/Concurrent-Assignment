8c5d6f87618ce2f8b22e3dc1079c311c3a6fbaf2
luxin <luxin.mymail@gmail.com>
2020-05-10 17:41:17 +0800

    Add Snake methods

--- SNAKE/src/Allsnake/Snake.java
+++ SNAKE/src/Allsnake/Snake.java
@@ -171,6 +171,137 @@ public class Snake {
 	 */
 	public void gameOver() {
 		game_over = true;
+
+	}
+
+	public void placeBonus(int bonus_type) {
+		int x = (int) (Math.random() * 1000) % gameSize;
+		int y = (int) (Math.random() * 1000) % gameSize;
+		if (map.getMapInfo(x, y) == EMPTY) {
+			map.setMapInfo(x, y,bonus_type);
+		} else {
+			placeBonus(bonus_type);
+		}
+	}
+	public void placeMalus(int malus_type) {
+		int x = (int) (Math.random() * 1000) % gameSize;
+		int y = (int) (Math.random() * 1000) % gameSize;
+		if (map.getMapInfo(x, y) == EMPTY) {
+			map.setMapInfo(x, y,malus_type);
+		} else {
+			placeMalus(malus_type);
+		}
+	}
+	public synchronized void moveSnake() {
+		if (direction < 0) {
+			return;
+		}
+		int ymove = 0;
+		int xmove = 0;
+		switch (direction) {
+			case UP:
+				xmove = 0;
+				ymove = -1;
+				break;
+			case DOWN:
+				xmove = 0;
+				ymove = 1;
+				break;
+			case RIGHT:
+				xmove = 1;
+				ymove = 0;
+				break;
+			case LEFT:
+				xmove = -1;
+				ymove = 0;
+				break;
+			default:
+				xmove = 0;
+				ymove = 0;
+				break;
+		}
+		int tempx = this.getSnakeInfo(0, 0);
+		int tempy = this.getSnakeInfo(0, 1);
+		int fut_x = this.getSnakeInfo(0, 0) + xmove;
+		int fut_y = this.getSnakeInfo(0, 1) + ymove;
+		if (fut_x < 0)
+			fut_x = gameSize - 1;
+		if (fut_y < 0)
+			fut_y = gameSize - 1;
+		if (fut_x >= gameSize)
+			fut_x = 0;
+		if (fut_y >= gameSize)
+			fut_y = 0;
+		if (map.getMapInfo(fut_x, fut_y)== FOOD_BONUS) {
+			Length++;
+			score++;
+			placeBonus(FOOD_BONUS);
+		}
+		if (map.getMapInfo(fut_x, fut_y) == FOOD_MALUS) {
+			Length += 2;
+			score--;
+		} else if (map.getMapInfo(fut_x, fut_y) == BIG_FOOD_BONUS) {
+			Length += 3;
+			score += 3;
+		}
+		this.setSnakeInfo(0, 0, fut_x);
+		this.setSnakeInfo(0, 1, fut_y);
+		if ((map.getMapInfo(this.getSnakeInfo(0, 0), this.getSnakeInfo(0, 1)) == SNAKE)) {
+			gameOver();
+			return;
+		}
+		map.setMapInfo(tempx, tempy, EMPTY);
+		int snakex, snakey, i;
+		for (i = 1; i < gameSize * gameSize; i++) {
+			if ((this.getSnakeInfo(i, 0) < 0) || (this.getSnakeInfo(i, 0) < 0)) {
+				break;
+			}
+			map.setMapInfo(this.getSnakeInfo(i, 0), this.getSnakeInfo(i, 1), EMPTY);
+			snakex = this.getSnakeInfo(i, 0);
+			snakey = this.getSnakeInfo(i, 1);
+			this.setSnakeInfo(i, 0, tempx);
+			this.setSnakeInfo(i, 1, tempy);
+			tempx = snakex;
+			tempy = snakey;
+		}
+		for (i = 0; i < gameSize * gameSize; i++) {
+			if ((this.getSnakeInfo(i, 0) < 0) || (this.getSnakeInfo(i, 1) < 0)) {
+				break;
+			}
+			map.setMapInfo(this.getSnakeInfo(i, 0), this.getSnakeInfo(i, 1), SNAKE);
+		}
+		bonusTime = bonusTime -1;
+		if (bonusTime == 0) {
+			for (i = 0; i < gameSize; i++) {
+				for (int j = 0; j < gameSize; j++) {
+					if (map.getMapInfo(i, j)== BIG_FOOD_BONUS)
+						map.setMapInfo(i, j, EMPTY);
+				}
+			}
+		}
+		malusTime = malusTime-1;
+		if (malusTime == 0) {
+			for (i = 0; i < gameSize; i++) {
+				for (int j = 0; j < gameSize; j++) {
+					if (map.getMapInfo(i, j) == FOOD_MALUS)
+						map.setMapInfo(i, j, EMPTY);
+				}
+			}
+		}
+		if (Length > 0) {
+			this.setSnakeInfo(i, 0, tempx);
+			this.setSnakeInfo(i, 1, tempx);
+			map.setMapInfo(this.getSnakeInfo(i, 0), this.getSnakeInfo(i, 1), SNAKE);
+			if (score % 10 == 0) {
+				placeBonus(BIG_FOOD_BONUS);
+				bonusTime = 100;
+			}
+			if (score % 5 == 0) {
+				placeMalus(FOOD_MALUS);
+				malusTime = 100;
+			}
+			Length--;
+		}
 	}
 
 	public void setSnakeInfo(int index1, int index2, int num) {
