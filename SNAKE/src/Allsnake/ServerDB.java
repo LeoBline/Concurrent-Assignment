7ece39726e076a838b0b2cff08751fb92c6be10c
WangJiaCheng1999 <wanjy171@mymail.unisa.edu.au>
2020-06-10 11:36:30 +0800

    Remove unnecessary code in Login() and add full comments.

--- SNAKE/src/Allsnake/ServerDB.java
+++ SNAKE/src/Allsnake/ServerDB.java
@@ -5,67 +5,74 @@ import org.mapdb.DBMaker;
 
 import java.io.File;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentNavigableMap; 
+import java.util.concurrent.ConcurrentNavigableMap;
 
+/**
+ *ServerDB class be created as a callable thread will return a value to
+ * show whether this account is existed in DB
+ */
 public class ServerDB implements Callable<String> {
-        // configure and open database using builder pattern. 
-        // all options are available with code auto-completion. 
-		DB db = DBMaker.newFileDB(new File("PlayerInformation"))
-				.closeOnJvmShutdown()
-				.encryptionEnable("password")
-				.make();
-	ConcurrentNavigableMap<String, String> Playermap = db.getTreeMap("playerinformation");
-	private String id,password;
+	//Get player account for information in FileDB "PlayerInformation"
+	DB db = DBMaker.newFileDB(new File("PlayerInformation"))
+			.closeOnJvmShutdown()
+			.encryptionEnable("password")
+			.make();
+	//Put the account information to the playerMap
+	ConcurrentNavigableMap<String, String> playerMap = db.getTreeMap("playerinformation");
+	private String id;
+	private String password;
+
 	public ServerDB(String id,String password) {
 		this.id=id;
 		this.password = password;
 	}
- 
-    public synchronized String Login(String id,String password) {
-    	//every time login will open the file db when login succeess or file will close the db.
 
-		DB db = DBMaker.newFileDB(new File("PlayerInformation"))
-				.closeOnJvmShutdown()
-				.encryptionEnable("password")
-				.make();
-		ConcurrentNavigableMap<String, String> Playermap = db.getTreeMap("playerinformation");
-    	
-    	if(Playermap.containsKey(id)) {
-    		if(Playermap.get(id).equals(password)) {
+
+	public String call() throws Exception {
+		// TODO Auto-generated method stub
+		return Login(id, password);
+	}
+
+	/**
+	 * Synchronized method to allow concurrent login
+	 * @param id
+	 * @param password
+	 * @return If login successfully return id, else return "".
+	 */
+    public synchronized String Login(String id,String password) {
+    	if(playerMap.containsKey(id)) {
+    		if(playerMap.get(id).equals(password)) {
     			db.close();
     			return id;
-    		}else
-    		{db.close();
-    			return "";
-    		}
-    	}else
-    	{
+    		}else {db.close();
+    		return ""; }
+    	}else {
     		db.close();
-    		return "";
-    	}
-    	
+    		return ""; }
     }
-    public synchronized ConcurrentNavigableMap<String, String> getMap(){
-    	return Playermap;
-    }
-    public synchronized DB getDB(){
-    	return db;
-    }
-    public void Updata(ConcurrentNavigableMap<String, String> Playermap,DB db) {
-    	if (!Playermap.containsKey("001")) {
-    		Playermap.put("001","123456");
-    	    Playermap.put("002","123456");
+
+	/**
+	 * Add some accounts into the map for login
+	 * @param map
+	 * @param db
+	 */
+	public void Update(ConcurrentNavigableMap<String, String> map, DB db) {
+    	if (!map.containsKey("001")) {
+			map.put("001","123456");
+			map.put("002","123456");
 		}
     	db.commit();
     }
-    public synchronized void end(DB db) {
-    	db.close();
-    }
 
-		@Override
-		public String call() throws Exception {
-			// TODO Auto-generated method stub
-			return Login(id, password);
-		}
+//Getters
+	public synchronized ConcurrentNavigableMap<String, String> getMap(){
+		return playerMap;
+	}
+
+	public synchronized DB getDB(){
+		return db;
+	}
+
+
 
 }
