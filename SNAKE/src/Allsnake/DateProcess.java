c4e510e6ecb65d4f9ce0e0911e872c5485b430b9
luxin <luxin.mymail@gmail.com>
2020-06-13 23:31:23 +0800

    add Data notes

--- SNAKE/src/Allsnake/DateProcess.java
+++ SNAKE/src/Allsnake/DateProcess.java
@@ -1,18 +1,32 @@
 package Allsnake;
 
-
+/**
+ * Update snake data imlement multithreading
+ */
 public class DateProcess implements Runnable {
-private Player[] playerlist;	
+	//The player array
+private Player[] playerlist;
+    //The start number of snakes
 private int star = 0;
+	//The end number of snakes
 private int end = 0;
 
-public DateProcess(Player[] playlist, int startNumber, int endNumber) {
+	/**
+	 * The game player count
+	 * @param playlist
+	 * @param startNumber
+	 * @param endNumber
+	 */
+	public DateProcess(Player[] playlist, int startNumber, int endNumber) {
 		// TODO Auto-generated constructor stub
 		playerlist = playlist;
 		star = startNumber;
 		end = endNumber;
 	}
 
+	/**
+	 * Multithreaded run method
+	 */
 	public void run() {
 		// TODO Auto-generated method stub
 		if(playerlist!=null) {
@@ -23,11 +37,6 @@ public DateProcess(Player[] playlist, int startNumber, int endNumber) {
 					playerlist[i].getPlayerBuffer().append(playerlist[i].getSnake().RandomDirection());
 				
 				}
-				
-					
-					
-
-				
 				playerlist[i].getSnake().setNext_direction(playerlist[i].getPlayerBuffer().take(playerlist[i].getSnake().getDirection()));
 				playerlist[i].getSnake().setDirection(playerlist[i].getSnake().getNext_direction());
 				playerlist[i].getSnake().moveSnake();
