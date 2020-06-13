0bbc91d6a44c4f989df209fefbb25f9560d36668
luxin <luxin.mymail@gmail.com>
2020-06-13 23:05:29 +0800

    add map notes

--- SNAKE/src/Allsnake/Map.java
+++ SNAKE/src/Allsnake/Map.java
@@ -1,33 +1,64 @@
 package Allsnake;
 
+/**
+ * The map class
+ */
 public class Map {
-//make Map class a singleton patten
-	
+//Make Map class a singleton patten
+	//Initialize map
 	private static Map map = new Map();
+	//Set coordinates array
 	private int[][] grid = null;
+	//Set game size
 	private int gameSize = 100;
 
-
+	/**
+	 * Set game grid
+	 */
 	public Map() {
 		grid = new int[gameSize][gameSize];
 	}
-	
+
+	/**
+	 *Get map
+	 * @return map
+	 */
 	public static  Map getMap() {
 		return map;
 	}
 
+	/**
+	 * Return map grid
+	 * @return grid
+	 */
 	public int[][] getGrid() {
 		return grid;
 	}
 
+	/**
+	 * Get game size
+	 * @return gameSize
+	 */
 	public int getGameSize(){
 		return gameSize;
 	}
 
+	/**
+	 * Set map information
+	 * @param index1
+	 * @param index2
+	 * @param num
+	 */
 	public synchronized void setMapInfo(int index1, int index2, int num) {
 		grid[index1][index2] = num;
 	}
-	
+
+	/**
+	 * Get map information
+	 * @param index1
+	 * @param index2
+	 * @return
+	 */
 	public synchronized int getMapInfo(int index1, int index2) {
 		return grid[index1][index2];
 	}
