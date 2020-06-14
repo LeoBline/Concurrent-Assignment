9d653db627fd16c7f207f66e31c3643fe8c3539f
WangJiaCheng1999 <yojc1999@gmail.com>
2020-06-15 01:14:22 +0800

    Second player test bug occurs

--- SNAKE/JunitTest/gameServerTest.java
+++ SNAKE/JunitTest/gameServerTest.java
@@ -20,49 +20,86 @@ ServerUIControl testServer = new ServerUIControl("test");
     public void testServerThread() throws InterruptedException {
         testServer.playerLogin("001","123456");
         testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
-        Snake testSnake = testServer.getPlayerList()[0].getSnake();
-        Map testMap = testServer.getMap();
 
         //UP is 0, DOWN = 1; LEFT = 2; RIGHT = 3;
         Assert.assertEquals(0,testServer.getPlayerList()[0].getPlayerBuffer().take(10));
         //Add a snake's movement to buffer
         testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
-        int snakeY = testSnake.getSnakeInfo(0,1);
+        int snakeY = testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1);
         testServer.updateSnake();
         //wait the thread pool finish job;
-        Thread.sleep(100);
+        Thread.sleep(10);
         //To determine whether the snake in the direction of the chang
-        Assert.assertEquals(0,testSnake.getDirection());
+        Assert.assertEquals(0,testServer.getPlayerList()[0].getSnake().getDirection());
         //To determine whether the snake y position change or not
-        Assert.assertEquals(snakeY-1,testSnake.getSnakeInfo(0,1));
+        Assert.assertEquals(snakeY-1,testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1));
 //         testServer = new ServerUIControl("test");
         //change all grid note to bonus
 
-        Thread.sleep(100);
+        Thread.sleep(10);
 
-
-        testServer.getPlayerList()[0].getSnake().placeBonus(testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,0),testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1)-1);
-        testServer.getPlayerList()[0].getSnake().placeBonus(testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,0),testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1)-2);
-        testServer.getPlayerList()[0].getSnake().placeBonus(testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,0),testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1)-3);
+        //Place 3 1 point bonuses in front of the first snake
+        testServer.getPlayerList()[0].getSnake().placeBonus(testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,0),
+                testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1)-1);
+        testServer.getPlayerList()[0].getSnake().placeBonus(testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,0),
+                testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1)-2);
+        testServer.getPlayerList()[0].getSnake().placeBonus(testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,0),
+                testServer.getPlayerList()[0].getSnake().getSnakeInfo(0,1)-3);
 
         testServer.updateSnake();
-        Thread.sleep(100);
+        Thread.sleep(10);
 
-//        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
+        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
         testServer.updateSnake();
-        Thread.sleep(100);
-//        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
+        Thread.sleep(10);
+        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
         testServer.updateSnake();
-        Thread.sleep(100);
-//        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
+        Thread.sleep(10);
+        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_UP);
         testServer.updateSnake();
-        Thread.sleep(100);
+        Thread.sleep(10);
 
+        //Test whether snake do get the bonus
         Assert.assertEquals(4,testServer.getPlayerList()[0].getSnake().getRealLength());
-//
 
-//
-//        Assert.assertEquals(true,testSnake.getGameover());
+        //Login second player
+        testServer.playerLogin("002","123456");
+        testServer.getPlayerList()[1].judgeInput(KeyEvent.VK_UP);
 
+        //See whether the player's action added into buffer
+        Assert.assertEquals(0,testServer.getPlayerList()[1].getPlayerBuffer().take(10));
+
+        //Place 2 1 point bonuses in front of the second snake
+        testServer.getPlayerList()[1].getSnake().placeBonus(testServer.getPlayerList()[1].getSnake().getSnakeInfo(0,0),
+                testServer.getPlayerList()[1].getSnake().getSnakeInfo(0,1)-1);
+        testServer.getPlayerList()[1].getSnake().placeBonus(testServer.getPlayerList()[1].getSnake().getSnakeInfo(0,0),
+                testServer.getPlayerList()[1].getSnake().getSnakeInfo(0,1)-2);
+
+
+        testServer.getPlayerList()[1].judgeInput(KeyEvent.VK_UP);
+        testServer.updateSnake();
+        Thread.sleep(10);
+        testServer.getPlayerList()[1].judgeInput(KeyEvent.VK_UP);
+        testServer.updateSnake();
+        Thread.sleep(10);
+
+        //Whether snake get points
+        Assert.assertEquals(3,testServer.getPlayerList()[1].getSnake().getRealLength());
+
+
+
+        //Simulate a snake walking in a circle,which will cause the dead of the snake
+        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_RIGHT);
+        testServer.updateSnake();
+        Thread.sleep(10);
+        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_DOWN);
+        testServer.updateSnake();
+        Thread.sleep(10);
+        testServer.getPlayerList()[0].judgeInput(KeyEvent.VK_LEFT);
+        testServer.updateSnake();
+        Thread.sleep(10);
+
+        //Whether the snake is dead
+        Assert.assertEquals(true,testServer.getPlayerList()[0].getSnake().getGameover());
     }
 }
